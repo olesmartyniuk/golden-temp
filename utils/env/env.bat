246c579235637867f8c4d4e661e36f7affc6dad0
???????? ? <???????? ?.@localhost>
2013-06-16 22:12:54 +0300

    + ????? ???????? ??? ?????-??????? + ????? ?????? "?????????????" ?? ????? ????????

--- utils/env/env.bat
+++ utils/env/env.bat
@@ -6,6 +6,7 @@ setx GT_PRODUCT "%1\product"
 setx GT_BIN "%1\product\bin"
 setx GT_SOURCES "%1\sources"
 setx GT_COMMON "%1\sources\common"
+setx GT_IMPLEMENT "%1\sources\implement"
 setx GT_IMPORTS "%1\imports"
 
 echo Environment variables for Golden Temp installed succesfuly
