0488b67f9fb505f2c18eb73421a155c745a30ab6
???????? ? <???????? ?.@localhost>
2013-06-25 22:22:36 +0300

    * ????????? ?????? ???? ??? ???????? ????? SOAP * ????? ??????? ???? ??? ???? ????? ???????? ?????

--- sources/common/uRemotable.pas
+++ sources/common/uRemotable.pas
@@ -90,102 +90,137 @@ type
   TStudents = array of TStudent;
 
   TTest = class(TRemotable)
-    public
-      Id: Integer;
-      Name: string;
-      MixQuestions: Boolean;
-      MixVariants: Boolean;
-      TimeLimit: Word;
-      Accessibility: Boolean;
-      CanClose: Boolean;
-      Greeting: string;
-      Congratulations: string;
-      Scales: string;
-      /// <link>association</link>
-      /// <clientQualifier>1</clientQualifier>
-      /// <supplierQualifier>N</supplierQualifier>
-      /// <directed>True</directed>
-      /// <label>включає</label>
-      Question: TQuestion;
-      /// <clientQualifier>M</clientQualifier>
-      /// <supplierQualifier>N</supplierQualifier>
-      /// <label>знаходиться</label>
-      Group: TGroup;
+    private
+      FId: Integer;
+      FName: string;
+      FMixQuestions: Boolean;
+      FMixVariants: Boolean;
+      FCongratulations: string;
+      FCanClose: Boolean;
+      FGreeting: string;
+      FTeacherId: Integer;
+      FScales: string;
+      FAccessibility: Boolean;
+      FTimeLimit: Word;
+    published
+      property Id: Integer read FId write FId;
       /// <supplierQualifier>1</supplierQualifier>
       /// <clientQualifier>N</clientQualifier>
       /// <label>створюється</label>
-      Teacher: TTeacher;
+      property TeacherId: Integer read FTeacherId write FTeacherId;
+      property Name: string read FName write FName;
+      property MixQuestions: Boolean read FMixQuestions write FMixQuestions;
+      property MixVariants: Boolean read FMixVariants write FMixVariants;
+      property TimeLimit: Word read FTimeLimit write FTimeLimit;
+      property Accessibility: Boolean read FAccessibility write FAccessibility;
+      property CanClose: Boolean read FCanClose write FCanClose;
+      property Greeting: string read FGreeting write FGreeting;
+      property Congratulations: string read FCongratulations write FCongratulations;
+      property Scales: string read FScales write FScales;
   end;
 
   TTests = array of TTest;
 
+  TQuestion = class(TRemotable)
+    private
+      FId: Integer;
+      FTestId: Integer;
+      FName: string;
+      FNumber: Integer;
+      FWeight: Integer;
+      FQuestionType: Integer;
+      FTopic: string;
+      FContent: Boolean;
+    published
+      property Id: Integer read FId write FId;
+      property TestId: Integer read FTestId write FTestId;
+      property Name: string read FName write FName;
+      property Number: Integer read FNumber write FNumber;
+      property Weight: Integer read FWeight write FWeight;
+      property QuestionType: Integer read FQuestionType write FQuestionType;
+      property Topic: string read FTopic write FTopic;
+      property Content: Boolean read FContent write FContent;
+  end;
+
+  TQuestions = array of TQuestion;
+
   TVariant = class(TRemotable)
-    public
-      Id: Integer;
-      Number: Word;
-      Text: string;
-      Correct: Boolean;
+    private
+      FCorrect: Boolean;
+      FId: Integer;
+      FQuestionId: Integer;
+      FNumber: Word;
+      FText: string;
+    published
+      property Id: Integer read FId write FId;
+      property Number: Word read FNumber write FNumber;
+      property Text: string read FText write FText;
+      property Correct: Boolean read FCorrect write FCorrect;
       /// <supplierQualifier>1</supplierQualifier>
       /// <clientQualifier>N</clientQualifier>
       /// <directed>True</directed>
       /// <label>належить</label>
-      Question: TQuestion;
+      property QuestionId: Integer read FQuestionId write FQuestionId;
   end;
 
   TVariants = array of TVariant;
 
   TAnswer = class(TRemotable)
-    public
-      Id: Integer;
-      TimeSpent: Integer;
-      Text: string;
-      Ball: Integer;
-      /// <clientCardinality>N</clientCardinality>
-      /// <supplierQualifier>1</supplierQualifier>
-      /// <directed>True</directed>
-      /// <label>належить</label>
-      Session: TSession;
+    private
+      FSessionId: Integer;
+      FId: Integer;
+      FTimeSpent: Integer;
+      FQuestionId: Integer;
+      FText: string;
+      FBall: Integer;
+    published
+      property Id: Integer read FId write FId;
       /// <clientQualifier>N</clientQualifier>
       /// <supplierQualifier>1</supplierQualifier>
       /// <label>дається</label>
       /// <directed>True</directed>
-      Question: TQuestion;
+      ///
+      property QuestionId: Integer read FQuestionId write FQuestionId;
+      /// <clientCardinality>N</clientCardinality>
+      /// <supplierQualifier>1</supplierQualifier>
+      /// <directed>True</directed>
+      /// <label>належить</label>
+      property SessionId: Integer read FSessionId write FSessionId;
+      property TimeSpent: Integer read FTimeSpent write FTimeSpent;
+      property Text: string read FText write FText;
+      property Ball: Integer read FBall write FBall;
   end;
 
   TAnswers = array of TAnswer;
 
-  TQuestion = class(TRemotable)
-    public
-      Id: Integer;
-      Name: string;
-      Number: Integer;
-      Weight: Integer;
-      QuestionType: Integer;
-      Topic: string;
-      Content: Boolean;
-  end;
-
-  TQuestions = array of TQuestion;
-
   TSession = class(TRemotable)
-    public
-      Id: Integer;
-      Active: Boolean;
-      LifeTime: Integer;
-      Closed: Integer;
-      StartTime: TDateTime;
-      FinishTime: TDateTime;
+    private
+      FClosed: Integer;
+      FStartTime: TDateTime;
+      FTestId: Integer;
+      FId: Integer;
+      FLifeTime: Integer;
+      FActive: Boolean;
+      FStudentId: Integer;
+      FFinishTime: TDateTime;
+    published
+      property Id: Integer read FId write FId;
       /// <link>association</link>
       /// <directed>True</directed>
       /// <label>включає</label>
       /// <clientQualifier>N</clientQualifier>
       /// <supplierQualifier>1</supplierQualifier>
-      Test: TTest;
+      property TestId: Integer read FTestId write FTestId;
       /// <directed>True</directed>
       /// <supplierQualifier>1</supplierQualifier>
       /// <clientQualifier>N</clientQualifier>
       /// <label>створюється</label>
-      Student: TStudent;
+      property StudentId: Integer read FStudentId write FStudentId;
+      property Active: Boolean read FActive write FActive;
+      property LifeTime: Integer read FLifeTime write FLifeTime;
+      property Closed: Integer read FClosed write FClosed;
+      property StartTime: TDateTime read FStartTime write FStartTime;
+      property FinishTime: TDateTime read FFinishTime write FFinishTime;
   end;
 
   TSessions = array of TSession;
